7094e596795634f14f59edc5f4af963020731107
JRS296 <jonathansamuel296@gmail.com>
2022-06-23 17:34:54 +0530

    Commit #2 - Creation + Demo of state in task.js - 23.06.2022 @17:34

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,6 @@
 import Task from './Task'
-
+import { useState } from 'react' //This is a hook
+/*
 //We want tasks to be part of state: Determines how object is rendered and behaves.
 const tasks = [
   {
@@ -20,13 +21,35 @@ const tasks = [
     day: 'Feb 5th at 2:30pm',
     reminder: false,
   },
-]
+]*/
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+      },
+    ]
+  )
   return (
     <>
       {tasks.map((task, index) => (
-        <h3>{task.text}</h3>
+        <h3 key={task.id}>{task.text}</h3>
         //<Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
       ))}
     </>
